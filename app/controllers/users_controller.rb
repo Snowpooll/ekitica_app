f7eb0fc470b6747cd464e84e6726b5e9a93da601
snowpool <snowpoollovely@gmail.com>
2016-10-24 22:23:02 +0900

    app rename

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,68 @@
+class UsersController < ApplicationController
+  before_action :set_params, only:[:edit,:update,:show, :followings,:followers]
+  before_action :correct_user, only:[:edit,:update]
+  
+
+  def show
+
+   @user = User.find(params[:id])
+   @microposts = @user.microposts.order(created_at: :desc).page(params[:page])
+
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] ="Wellcome to the Sample App"
+      redirect_to @user # ここを修正
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "プロフィールを更新しました"
+      redirect_to @user 
+    else
+      render 'edit'
+    end
+  end
+  
+  def followings
+    @title = 'following users'
+ #   @user =  User.find(params[:id]) #before_action で定義のため不要
+    @users = @user.following_users.page(params[:page])
+    render 'follow_follower_list'
+  end
+  
+  def followers
+    @title = 'follower users' #タイトル表示の文字列
+  #  @user = User.find(params[:id])
+    @users = @user.follower_users
+    render 'follow_follower_list'
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :location,:password,:password_confirmation,:avatar)
+  end
+  
+  def set_params
+    @user = User.find(params[:id])
+  end
+  
+  def correct_user
+    redirect_to root_path if !current_user?(@user)
+  end
+  
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,46 @@
+class User < ActiveRecord::Base
+  before_save {self.email = self.email.downcase}
+  validates :name, presence: true,length:{maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length:{maximum: 255},format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :location, absence: true, 
+                       on: :create
+  validates :location, allow_blank: true, 
+                       length: { minimum: 2, maximum: 20 }, 
+                       on: :update
+  has_secure_password
+  has_many :microposts
+  
+  has_many :following_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  
+  has_many :following_users, through: :following_relationships, source: :followed
+  
+  has_many :follower_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  
+  has_many :follower_users, through: :follower_relationships, source: :follower
+  
+  #uploader 
+  mount_uploader :avatar, AvatarUploader
+  
+  
+ #他のユーザのフォロー
+ def follow(other_user)
+   following_relationships.find_or_create_by(followed_id: other_user.id)
+ end
+ 
+ #フォローしているユーザをアンフォロー
+ def unfollow(other_user)
+   following_relationship = following_relationships.find_by(followed_id: other_user.id)
+   following_relationship.destroy if following_relationship
+ end
+ 
+ #あるユーザをフォローいているか？
+ def following?(other_user)
+   following_users.include?(other_user)
+ end
+ 
+ #つぶやき取得
+ def feed_items
+   Micropost.where(user_id: following_user_ids + [self.id])
+ end
+end
