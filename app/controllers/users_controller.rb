6630b91f1fb71ab6dde82d470183c37204b4e8f5
tomoaki-kimura <tkim19750404@gmail.com>
2016-10-27 00:50:37 +0900

    fix devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_params, only:[:edit,:update,:show, :followings,:followers]
-  before_action :correct_user, only:[:edit,:update]
+  # before_action :correct_user, only:[:edit,:update]
   
 
   def show
@@ -10,31 +10,31 @@ class UsersController < ApplicationController
 
   end
   
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:success] ="Wellcome to the Sample App"
-      redirect_to @user # ここを修正
-    else
-      render 'new'
-    end
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     flash[:success] ="Wellcome to the Sample App"
+  #     redirect_to @user # ここを修正
+  #   else
+  #     render 'new'
+  #   end
+  # end
   
-  def edit
-  end
+  # def edit
+  # end
   
-  def update
-    if @user.update(user_params)
-      flash[:success] = "プロフィールを更新しました"
-      redirect_to @user 
-    else
-      render 'edit'
-    end
-  end
+  # def update
+  #   if @user.update(user_params)
+  #     flash[:success] = "プロフィールを更新しました"
+  #     redirect_to @user 
+  #   else
+  #     render 'edit'
+  #   end
+  # end
   
   def followings
     @title = 'following users'
